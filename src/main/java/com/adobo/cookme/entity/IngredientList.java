fff72fea9304d44c9e241e754f1e09d770300dc2
mark.baje <mark.baje@sanyutechnos-ph.com>
2024-06-20 09:23:20 +0800

    Replaced Entity's classes to records

--- src/main/java/com/adobo/cookme/entity/IngredientList.java
+++ src/main/java/com/adobo/cookme/entity/IngredientList.java
@@ -1,34 +1,4 @@
 package com.adobo.cookme.entity;
 
-public class IngredientList{
-    private Long idIngredient;
-    private String strIngredient;
-    private String strDescription;
-
-    public IngredientList() {
-    }
-
-    public Long getIdIngredient() {
-        return idIngredient;
-    }
-
-    public void setIdIngredient(Long idIngredient) {
-        this.idIngredient = idIngredient;
-    }
-
-    public String getStrIngredient() {
-        return strIngredient;
-    }
-
-    public void setStrIngredient(String strIngredient) {
-        this.strIngredient = strIngredient;
-    }
-
-    public String getStrDescription() {
-        return strDescription;
-    }
-
-    public void setStrDescription(String strDescription) {
-        this.strDescription = strDescription;
-    }
+public record IngredientList(Long idIngredient, String strIngredient, String strDescription) {
 }
